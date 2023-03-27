3378b68bf4a350dc6703e3be6c31872951f1138e
Viktoryia Stoliar <vikynyshka88genya@gmail.com>
2023-03-27 23:09:28 +0300

    OOP

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const time = document.querySelector('.time');
-const data = document.querySelector('p');
-function doData(time_, data_) {
-    
-    data_.innerHTML = `since. ${new Date().getFullYear()}`;
-    time_.innerHTML = `${new Date().getHours()} : ${new Date().getMinutes()} : ${new Date().getSeconds()}`;
+class AppleWatch {
 
+    constructor() {
+        setInterval(this.doData, 1000)
+    }
+
+    doData() {
+        const time = document.querySelector('.time');
+        const data = document.querySelector('p');
+        const date = new Date();
+        data.innerHTML = `since. ${date.getFullYear()}`;
+        // data.innerHTML = 
+        // time.innerHTML = `${date.getHours()} : ${date.getMinutes()} : ${date.getSeconds()}`;
+        time.innerHTML = date.toLocaleTimeString('en')
+    }
 }
-setInterval(doData, 1000, time, data)
+
+const appleWatch = new AppleWatch();
